c8d6257c4a235eb31f217f0f4b64aa25322211f3
blenderfreaky <blendcrafterxiv@gmail.com>
2020-09-08 02:41:59 +0200

    Fix FileDescriptor Serialization

--- Pictua/FileDescriptor.cs
+++ Pictua/FileDescriptor.cs
@@ -1,37 +1,27 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace Pictua
 {
     public struct FileDescriptor : IEquatable<FileDescriptor>
     {
-        public string Extension { get; }
+        public string Extension { get; set; }
 
-        public byte[] ContentHash { get; }
-
-        public string ContentHashString => BitConverter.ToString(ContentHash).Replace("-", "").ToLowerInvariant();
-
-        public string UniqueName => $"{ContentHashString}{Extension}";
+        public string UniqueName { get; set; }
 
         public FileDescriptor(string extension, byte[] contentHash)
         {
             Extension = extension;
-            ContentHash = contentHash;
+            UniqueName = BitConverter.ToString(contentHash).Replace("-", "").ToLowerInvariant() + Extension;
         }
 
-        public FileDescriptor(string filePath)
-        {
-            Extension = Path.GetExtension(filePath);
-
-            ContentHash = FileHashes.CalculateMD5(filePath);
-        }
+        public FileDescriptor(string filePath) : this(Path.GetExtension(filePath), FileHashes.CalculateMD5(filePath)) { }
 
         public override bool Equals(object? obj) => obj is FileDescriptor descriptor && Equals(descriptor);
 
-        public bool Equals(FileDescriptor other) => Extension == other.Extension && ContentHash.SequenceEqual(other.ContentHash);
+        public bool Equals(FileDescriptor other) => Extension == other.Extension && UniqueName == other.UniqueName;
 
-        public override int GetHashCode() => HashCode.Combine(Extension, ContentHash, ContentHashString, UniqueName);
+        public override int GetHashCode() => HashCode.Combine(Extension, UniqueName);
 
         public static bool operator ==(FileDescriptor left, FileDescriptor right) => left.Equals(right);
 
